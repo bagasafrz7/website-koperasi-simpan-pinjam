b8135ae4064f89e739fd1504dbc6150c7ebf2e26
bagasafrz7 <bagasafrz16@gmail.com>
2024-12-27 09:36:29 +0700

    feat: remove select in table

--- app/admin/master-data/wilayah/city/_components/city-tables/index.tsx
+++ app/admin/master-data/wilayah/city/_components/city-tables/index.tsx
@@ -30,25 +30,25 @@ export default function CitiesTable({
   } = useCitiesTableFilters();
 
   const columns: ColumnDef<City>[] = [
-    {
-      id: 'select',
-      header: ({ table }) => (
-        <Checkbox
-          checked={table.getIsAllPageRowsSelected()}
-          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-          aria-label="Select all"
-        />
-      ),
-      cell: ({ row }) => (
-        <Checkbox
-          checked={row.getIsSelected()}
-          onCheckedChange={(value) => row.toggleSelected(!!value)}
-          aria-label="Select row"
-        />
-      ),
-      enableSorting: false,
-      enableHiding: false
-    },
+    // {
+    //   id: 'select',
+    //   header: ({ table }) => (
+    //     <Checkbox
+    //       checked={table.getIsAllPageRowsSelected()}
+    //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+    //       aria-label="Select all"
+    //     />
+    //   ),
+    //   cell: ({ row }) => (
+    //     <Checkbox
+    //       checked={row.getIsSelected()}
+    //       onCheckedChange={(value) => row.toggleSelected(!!value)}
+    //       aria-label="Select row"
+    //     />
+    //   ),
+    //   enableSorting: false,
+    //   enableHiding: false
+    // },
     {
       accessorKey: 'id',
       header: 'ID'
